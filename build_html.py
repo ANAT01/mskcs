c88749799ef5cf0e4a395ef24dcb82557704efa6
ANAT01 <anatoly@tobolda.ru>
2019-12-03 16:28:08 +0500

    Update to 0.1.3

--- build_html.py
+++ build_html.py
@@ -1,5 +1,31 @@
 from jinja2 import Environment, FileSystemLoader
 import os
+import glob, os
+import yaml
+import json
+
+# Собираем все *.CS.YAML в одну строку
+allcs = ''
+for file in glob.glob("data\*.cs.yaml"):
+    print(file)
+    with open(file, encoding='UTF-8') as infile:
+        for line in infile:
+           allcs += line
+
+# Собираем все *.ZONE.YAML в одну строку
+allzones = ''
+for file in glob.glob("data\*.zone.yaml"):
+    print(file)
+    with open(file, encoding='UTF-8') as infile:
+        for line in infile:
+            allzones += line
+
+# преобразуем YAML -> JSON -> строка
+csstr = json.dumps(yaml.safe_load(allcs), ensure_ascii=False)
+# грязный хак так как yaml ругаетсы на слэщ '\'
+zonestr = json.dumps(yaml.safe_load(allzones.replace('\\', "$$$")), ensure_ascii=False).replace('$$$', '\\')
+
+
 
 root = os.path.dirname(os.path.abspath(__file__))
 templates_dir = os.path.join(root, 'html', 'templates')
@@ -8,8 +34,10 @@ template = env.get_template('index.html')
 
 debug = False
 
-JSLIBS = open('html/js/jquery-3.4.1.min.js', 'r', encoding="utf8").read() + open('html/js/bootstrap.min.js', 'r', encoding="utf8").read()
-CSS = open('html/css/bootstrap.min.css', 'r', encoding="utf8").read() + open('html/css/custom.css', 'r', encoding="utf8").read()
+JSLIBS = open('html/js/jquery-3.4.1.min.js', 'r', encoding="utf8").read() + open('html/js/bootstrap.min.js', 'r',
+                                                                                 encoding="utf8").read()
+CSS = open('html/css/bootstrap.min.css', 'r', encoding="utf8").read() + open('html/css/custom.css', 'r',
+                                                                             encoding="utf8").read()
 JSDATA = open('tmp/data.js', 'r', encoding="utf8").read()
 JSAPP = open('html/js/app.js', 'r', encoding="utf8").read()
 VERSION = open('VERSION', 'r', encoding="utf8").read()
@@ -25,4 +53,6 @@ with open(filename, 'w', encoding="utf8") as fh:
         CSS=CSS,
         JSDATA=JSDATA,
         JSAPP=JSAPP,
+        ALLCS="var CSS =%s" % csstr,
+        ALLZONE="var CADASTRE =%s" % zonestr,
     ))
